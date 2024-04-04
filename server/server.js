27e22c56ec7a662e41a542830bf874c0861ae075
Jannie Hamberg <jg311209@live.com>
2024-04-04 20:56:01 +0200

    Server side basics

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
 
 const usersRouter = require('./resources/users/users.router');
 const authRouter = require('./resources/auth/auth.router');
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieSession({
+    secret: 's3cr3tk3y',
+    maxAge: 1000 * 60 * 60 * 24,
+}));
 
 app.use('/api/users', usersRouter);
 app.use('/api/auth', authRouter);
